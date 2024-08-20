4a51f208c3ad7c52ba34ce8ebd10cedd92d5e548
Atvempa <161967671+Atvempa@users.noreply.github.com>
2024-08-20 19:24:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from pymongo.mongo_client import MongoClient
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
@@ -49,6 +50,18 @@ def chat():
     
     data = request.json
     query_text = data.get('query')
+    
+    uri = "mongodb+srv://umlbot:Boston123@cluster0.mlwhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        query_text = "Pinged your deployment. You successfully connected to MongoDB!"
+    except Exception as e:
+        query_text = e
 
     # # Prepare the DB.
     # embedding_function = OpenAIEmbeddings()
